ad7bf1b6eb02f7ac7375c567ad18c39f309ff5b6
Michael Chau <mchau5@uic.edu>
2020-02-21 14:13:48 +0000

    test

---
+++ homework/canvas-demo.js
@@ -0,0 +1,23 @@
+// canvas-demo.js
+
+const demoCanvas = document.getElementById('canvas-demo').getContext('2d');
+demoCanvas.width = window.innerWidth;
+demoCanvas.height = window.innerHeight;
+window.onload = function() {// make sure to use onload
+   
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
