00bef2b9363e754222765fe264327173f773e7ff
Michael Chau <mchau5@uic.edu>
2020-03-09 14:46:57 +0000

    TopAppBarSpacing

--- homework/canvas-demo.js
+++ homework/canvas-demo.js
@@ -12,10 +12,15 @@ window.onload = function() {// make sure to use onload
 	
 	
 	
+	document.addEventListener("mousedown",
+(e) =>{
+    console.log(e)
+});
 	
 	
-	
-	
+	document.addEventListener("keydown",(e) => {
+    console.log(e);
+})
 	
 	
 	
